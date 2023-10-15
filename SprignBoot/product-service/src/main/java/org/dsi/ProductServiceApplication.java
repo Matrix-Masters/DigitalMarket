c4e6dcf885372037d9a72267d62f666f50a75401
TalelMejri <talelmejri8@gmail.com>
2023-10-16 00:32:51 +0100

    EnableFeignClients

--- SprignBoot/product-service/src/main/java/org/dsi/ProductServiceApplication.java
+++ SprignBoot/product-service/src/main/java/org/dsi/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package org.dsi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
