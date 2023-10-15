ad7053bdff33f9af9854d4abf4a85035b5b115d4
TalelMejri <talelmejri8@gmail.com>
2023-10-15 14:56:06 +0100

    fix Index.ts

---
+++ NodeJs/src/index.ts
@@ -0,0 +1,15 @@
+import app from "./config/app";
+import ConnectMongo from "./config/DbConnect"
+const eurekaHelper = require('./Setup_Euruka');
+const PORT = process.env.PORT || 3000;
+const uri:string='mongodb://localhost:27017/biblio'
+const connect=new ConnectMongo(uri);
+
+ connect.ConnectToBd().then(()=>{
+   connect.server(app,PORT);
+   eurekaHelper.registerWithEureka('nodeJs-service', PORT);
+ }).catch((error:any)=>{
+   console.log(error);
+ })
+
+
