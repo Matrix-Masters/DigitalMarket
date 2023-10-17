b294e8d1bc417e3fec33b4edb6011865eb4aef9e
TalelMejri <talelmejri8@gmail.com>
2023-10-17 23:36:56 +0100

    add Module Material to import all angularMaterial

---
+++ AngularFront/src/app/material/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  imports:[
+    MatIconModule,
+  ],
+
+  exports:[
+    MatIconModule,
+]
+
+})
+export class MaterialModule { }
