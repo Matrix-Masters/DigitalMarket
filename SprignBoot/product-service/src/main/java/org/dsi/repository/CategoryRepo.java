55db3aeba3119159237c8800869da9cf9174a0aa
ghassen <ghassenahmed74@gmail.com>
2023-10-20 22:43:29 +0100

    fix bugs

--- SprignBoot/product-service/src/main/java/org/dsi/repository/CategoryRepo.java
+++ SprignBoot/product-service/src/main/java/org/dsi/repository/CategoryRepo.java
@@ -1,10 +1,9 @@
 package org.dsi.repository;
 
-import java.util.List;
+
 
 import org.dsi.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
