52edc85bcfa4a06bae219298987a5f128b022c26
ghassen <ghassenahmed74@gmail.com>
2023-10-20 22:43:53 +0100

    add findProductById + getProductPaginate +  getProductPaginateSearch (queries)

--- SprignBoot/product-service/src/main/java/org/dsi/repository/ProductRepo.java
+++ SprignBoot/product-service/src/main/java/org/dsi/repository/ProductRepo.java
@@ -3,7 +3,8 @@ package org.dsi.repository;
 import java.util.List;
 
 import org.dsi.entity.Product;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -19,4 +20,13 @@ public interface ProductRepo extends JpaRepository<Product,Long> {
 	   
 	   @Query(value = "SELECT p FROM Product p WHERE p.id = :id")
 	   Product ProductWithId(long id);
+	   
+	   @Query(value="SELECT * FROM  product  WHERE id=:id",nativeQuery=true)
+		Product findProductById(Long id);
+		
+		@Query(value="select * from product where status=1 ",nativeQuery=true)
+		Page<Product> getProductPaginate(Pageable pageable );
+		
+		@Query(value="SELECT * FROM product WHERE status = 1 AND name LIKE :name ",nativeQuery=true)
+		Page<Product> getProductPaginateSearch(String name,Pageable pageable );
 }
