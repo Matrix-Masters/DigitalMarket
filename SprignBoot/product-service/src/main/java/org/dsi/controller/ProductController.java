fbe03470fe32dfa39e546ca23488a4f35c144f90
ghassen <ghassenahmed74@gmail.com>
2023-10-20 22:43:05 +0100

    add pagination + search + deleteAll + deleteById

--- SprignBoot/product-service/src/main/java/org/dsi/controller/ProductController.java
+++ SprignBoot/product-service/src/main/java/org/dsi/controller/ProductController.java
@@ -7,9 +7,12 @@ import org.dsi.repository.NodeSync;
 import org.dsi.repository.ProductRepo;
 import org.dsi.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,9 +21,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import Payload.PaginateInfo;
 import Payload.ProducInfo;
 import net.minidev.json.JSONObject;
 import java.lang.Long;
+import java.util.List;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -47,9 +53,38 @@ public class ProductController {
 	 }
 	
 	@GetMapping("/AllProduct")
-	public ResponseEntity<?> AllProducts(){
-		return ResponseEntity.ok(ProductRepo.findAll());
-	}
+	public ResponseEntity<?> getAllProductPaginate(
+    		@RequestParam(name="search",defaultValue="") String name,
+    		@RequestParam(name = "page", defaultValue = "0") int page,
+			@RequestParam(name = "per_page", defaultValue = "2") int size){
+    	
+	 	
+	 	if (page < 0 || size <= 0 ) {
+	        return ResponseEntity.badRequest().body("Invalid page or per_page values.");
+	 		}
+	    
+	 		try {
+	        Page<Product> product;
+	        if(name.isEmpty()==true) {
+	        	product=ProductRepo.getProductPaginate(PageRequest.of(page, size));
+	        } else {
+	        	product=ProductRepo.getProductPaginateSearch(name,PageRequest.of(page, size));
+	        }
+	        
+	        int total = product.getTotalPages();
+	        int[] count_page = new int[total];
+	        for (int i = 0; i < total; i++) {
+	            count_page[i] = i;
+	        }
+
+	        PaginateInfo data = new PaginateInfo(count_page, product, page);
+	        return ResponseEntity.ok(data);
+	        
+	    } catch (Exception e) {
+	    	
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while fetching paginated products.");
+	    }
+    }
 	
 	@GetMapping("/ProductsWithoutCategory")
 	public ResponseEntity<?> ProductsWithoutCategory(){
@@ -67,6 +102,48 @@ public class ProductController {
 		prod.setCategory(cat);
 		ProductRepo.save(prod);
 		return  ResponseEntity.ok().body("Product Upated");
+	}
+	
+	@DeleteMapping("/DeleteAll")
+	public ResponseEntity<?> DeleteAllProducts(){
+		List<Product> products = ProductRepo.findAll();
+		try {
+			if(products.isEmpty()) {
+				
+				return ResponseEntity.ok("No Products is available to delete , please check later");
+			}else {
+				
+				ProductRepo.deleteAll();
+
+				return ResponseEntity.ok("All Products deleted successfully");
+			}
+		} catch (SecurityException e) {
+			
+	        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Permission denied for this operation.");
+	        
+	    } catch (Exception e) {
+	    	
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while deleting products.");
+	    }
+	}
+	
+	@DeleteMapping("/DeleteProductById")
+	public ResponseEntity<?> DeleteProductById(@RequestParam("id") Long id){
+		Product product = ProductRepo.findProductById(id);
+		try {
+			if(product == null) {
+				String message = "This Product is not available , please check again";
+	            return ResponseEntity.ok(message);
+			}else {
+				ProductRepo.delete(product);
+				return ResponseEntity.ok("Product deleted successfully");
+			}
+		}catch(Exception e) {
+			
+			 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while deleting the product.");
+		}
+		
+		
 	}
 	
 
