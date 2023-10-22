9dc9f95ec0ed55d9cf84a2a61429454f00247dd5
ghassen <ghassenahmed74@gmail.com>
2023-10-22 15:14:22 +0100

    add categories array

--- AngularFront/src/app/home/navbar/navbar.component.ts
+++ AngularFront/src/app/home/navbar/navbar.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+
+  categories : any[] = ['SkinCare', 'Make up','Haire care','bath & body','Beauti supplements','Promos'];
+
 
 }
