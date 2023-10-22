acef8dd6911f38f1866032fbabfc6c7e28472aa6
TalelMejri <talelmejri8@gmail.com>
2023-10-22 18:19:07 +0100

    LibererProduct

--- NodeJs/src/Controller/ProductController.ts
+++ NodeJs/src/Controller/ProductController.ts
@@ -1,10 +1,10 @@
 
 import { Request, Response } from "express";
-import product from "../Model/Products";
+import Product from "../Model/Products";
 
-export const findAll =async (req:Request,res: Response) => {
+export const FindProductWithoutProduct =async (req:Request,res: Response) => {
   try{
-    const products=await product.find().exec();
+    const products = await Product.find({ status: 1 }).exec();
     res.status(200).json(products);  
   }catch(err){
     res.status(500).json({ error: 'Internal Server Error' });
@@ -12,7 +12,7 @@ export const findAll =async (req:Request,res: Response) => {
 }
 
 export const addProduct = async (req: Request, res: Response) => {
-  let prod = new product(req.body);
+  let prod = new Product(req.body);
   try {
       await prod.save() ;
       res.status(201).json(req.body);
@@ -20,4 +20,47 @@ export const addProduct = async (req: Request, res: Response) => {
     res.status(500).json({ error: err });
   }
 }
+
+
+
+export const LibererProduct = async (req: Request, res: Response) => {
+  try {
+    const product = await Product.findOneAndUpdate({idSpring:req.params.id}, { categoryId: null });
+    if (!product) {
+      return res.status(404).send("Product not found");
+    }
+    const updatedProduct = await Product.findOne({idSpring:req.params.id});
+    return res.status(200).send(updatedProduct);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+}
+
+export const UpdateIdProducts = async (req: Request, res: Response) => {
+  try {
+    const product = await Product.findOneAndUpdate({idSpring:req.params.id}, { categoryId: req.body.categoryId });
+    if (!product) {
+      return res.status(404).send("Product not found");
+    }
+    const updatedProduct = await Product.findOne({idSpring:req.params.id});
+    return res.status(200).send(updatedProduct);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+}
+
+export const RejectProduct = async (req: Request, res: Response) => {
+  try {
+    const product = await Product.findOneAndUpdate({idSpring:req.params.id}, { status: 2 });
+    if (!product) {
+      return res.status(404).send("Product not found");
+    }
+    const updatedProduct = await Product.findOne({idSpring:req.params.id});
+    return res.status(200).send(updatedProduct);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+}
+
+
 
