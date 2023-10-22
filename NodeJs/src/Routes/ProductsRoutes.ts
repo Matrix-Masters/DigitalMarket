1058c6de04e001db7759621701d2a9eaae843fb6
TalelMejri <talelmejri8@gmail.com>
2023-10-22 18:21:08 +0100

    UpdateIdProducts

--- NodeJs/src/Routes/ProductsRoutes.ts
+++ NodeJs/src/Routes/ProductsRoutes.ts
@@ -4,5 +4,6 @@ module.exports=(router:any)=>{
     router.get("/GetProducts",FindProductWithoutProduct),
     router.post("/addProduct",addProduct)
     router.put("/LibererProduct/:id",LibererProduct)
+    router.put("/UpdateIdProducts/:id",UpdateIdProducts)
     router.put("/RejectProduct/:id",RejectProduct)
 }
