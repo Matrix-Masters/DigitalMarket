4b0dbaf48d5c180e46ebe1857e1fc344c95a4e1d
ghassen <ghassenahmed74@gmail.com>
2023-10-23 11:58:37 +0100

    add images

--- AngularFront/src/app/home/home/home.component.ts
+++ AngularFront/src/app/home/home/home.component.ts
@@ -9,17 +9,12 @@ export class HomeComponent {
   images = [
     {
       imageSrc:
-        'https://images.unsplash.com/photo-1460627390041-532a28402358?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+        '../../../assets/ads.jpg',
       imageAlt: 'nature1',
     },
     {
       imageSrc:
-        'https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-      imageAlt: 'nature2',
-    },
-    {
-      imageSrc:
-        'https://images.unsplash.com/photo-1640844444545-66e19eb6f549?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
+      '../../../assets/ads.jpg',
       imageAlt: 'person1',
     },
     {
