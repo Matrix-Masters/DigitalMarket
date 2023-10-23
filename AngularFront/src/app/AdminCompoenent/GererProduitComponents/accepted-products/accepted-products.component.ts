9c687ad5ddd68586244aeb2e2de37e224b77d147
hkimi amin <hkimiamin02@gmail.com>
2023-10-23 23:39:07 +0100

    add get products functions

--- AngularFront/src/app/AdminCompoenent/GererProduitComponents/accepted-products/accepted-products.component.ts
+++ AngularFront/src/app/AdminCompoenent/GererProduitComponents/accepted-products/accepted-products.component.ts
@@ -7,6 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class AcceptedProductsComponent {
   @Input() type = '';
+  @Input() products=[];
   modalVisible: boolean = false;
 
   showProductInfo() {
