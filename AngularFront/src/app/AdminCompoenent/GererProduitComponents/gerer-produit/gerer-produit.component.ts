a237eba24e2b4a6f38d1e4863bb80e3bc2835c5e
hkimi amin <hkimiamin02@gmail.com>
2023-10-23 11:17:01 +0100

    add gerer produit

---
+++ AngularFront/src/app/AdminCompoenent/GererProduitComponents/gerer-produit/gerer-produit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gerer-produit',
+  templateUrl: './gerer-produit.component.html',
+  styleUrls: ['./gerer-produit.component.scss']
+})
+export class GererProduitComponent {
+  accepted="accepted"
+  refused="refused"
+  pending="pending"
+}
