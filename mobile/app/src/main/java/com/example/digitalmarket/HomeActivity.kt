ef07fba22a0a49a23fd4211205cf4cd530ea4b10
TalelMejri <talelmejri8@gmail.com>
2023-10-24 20:38:13 +0100

    add ProjectMobile

---
+++ mobile/app/src/main/java/com/example/digitalmarket/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.digitalmarket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+    }
+}
