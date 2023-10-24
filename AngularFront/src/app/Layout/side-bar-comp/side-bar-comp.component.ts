914067dcd96e1eba4d038dcea2a4361c3cc42e01
TalelMejri <talelmejri8@gmail.com>
2023-10-24 20:39:03 +0100

    add sidebar

--- AngularFront/src/app/Layout/side-bar-comp/side-bar-comp.component.ts
+++ AngularFront/src/app/Layout/side-bar-comp/side-bar-comp.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar-comp',
   templateUrl: './side-bar-comp.component.html',
   styleUrls: ['./side-bar-comp.component.scss']
 })
-export class SideBarCompComponent {
+export class SideBarCompComponent implements OnInit {
+    OpenedSideBar:Boolean=true;
+    ngOnInit(): void {
+   
+    }
 
 }
