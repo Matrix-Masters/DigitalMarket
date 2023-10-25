d90df798d222d1d7937dff083d30de09e05a2875
hkimi amin <hkimiamin02@gmail.com>
2023-10-25 20:31:18 +0100

    clean code

--- mobile/app/src/main/java/com/example/digitalmarket/HomeActivity.kt
+++ mobile/app/src/main/java/com/example/digitalmarket/HomeActivity.kt
@@ -6,14 +6,14 @@ import android.os.Bundle
 import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
-    lateinit var btn_login:Button
+    //lateinit var btn_login:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home)
-        btn_login=findViewById(R.id.button)
+
+        /*btn_login=findViewById(R.id.button)
         btn_login.setOnClickListener{
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
-        }
+        }*/
     }
 }
