474ed446cc6b371d65bc0937afc8b0b64b73bbc6
hkimi amin <hkimiamin02@gmail.com>
2023-10-25 14:15:24 +0100

    add login activity

--- mobile/app/src/main/java/com/example/digitalmarket/HomeActivity.kt
+++ mobile/app/src/main/java/com/example/digitalmarket/HomeActivity.kt
@@ -1,11 +1,19 @@
 package com.example.digitalmarket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var btn_login:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
+        btn_login=findViewById(R.id.button)
+        btn_login.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
