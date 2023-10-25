a3f8b04fb85a0bce032e4e76fa4e6882d32bbd9b
Youssef Souissi <souissiyoussef9@gmail.com>
2023-10-25 23:16:48 +0100

    Intro screens

---
+++ mobile/app/src/main/java/com/example/digitalmarket/IntroScreen1.kt
@@ -0,0 +1,18 @@
+package com.example.digitalmarket
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class IntroScreen : AppCompatActivity() {
+    lateinit var navBtn: Button
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.intro_screen_2)
+
+ }
+
+}
---
+++ mobile/app/src/main/java/com/example/digitalmarket/IntroScreen3.kt
@@ -0,0 +1,4 @@
+package com.example.digitalmarket
+
+class IntroScreen3 {
+}
