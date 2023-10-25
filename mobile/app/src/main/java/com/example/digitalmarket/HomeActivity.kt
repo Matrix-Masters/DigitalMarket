0e07a5540398c4fa14d1b85a3620d488d2dddd49
ghassen <ghassenahmed74@gmail.com>
2023-10-25 19:32:37 +0100

    add method goSignup

--- mobile/app/src/main/java/com/example/digitalmarket/HomeActivity.kt
+++ mobile/app/src/main/java/com/example/digitalmarket/HomeActivity.kt
@@ -1,11 +1,33 @@
 package com.example.digitalmarket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var btnClient:Button
+    lateinit var btnSupplier:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
+        btnClient = findViewById(R.id.btnClient)
+        btnSupplier = findViewById(R.id.btnSupplier)
+
+        btnClient.setOnClickListener {
+            goSignup("client")
+        }
+
+        btnSupplier.setOnClickListener {
+            goSignup("supplier")
+        }
+
+
+    }
+
+    private fun goSignup(role:String){
+        var intent = Intent(this,signup::class.java)
+        intent.putExtra("role",role)
+        startActivity(intent)
     }
 }
