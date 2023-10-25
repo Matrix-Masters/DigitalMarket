e0971623445209db23e7f6274b7b9aeeb2b58b7e
hkimi amin <hkimiamin02@gmail.com>
2023-10-25 20:25:03 +0100

    add library

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.google.android.material:material:1.4.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
