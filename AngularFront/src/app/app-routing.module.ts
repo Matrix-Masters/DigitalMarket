25f3cc4141a8875799231f8d8bc5c64ee711a74e
TalelMejri <talelmejri8@gmail.com>
2023-10-25 23:40:47 +0100

    add dash component

--- AngularFront/src/app/app-routing.module.ts
+++ AngularFront/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClasserProductComponent } from './AdminCompoenent/classer-product/classer-product.component';
 import { HomeComponent } from './home/home/home.component';
+import { DashboardComponent } from './AdminCompoenent/dashboard/dashboard.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
-  {path:'classer',component:ClasserProductComponent}
+  {path:'classer',component:ClasserProductComponent},
+  {path:"dash",component:DashboardComponent}
 ]
 
 @NgModule({
