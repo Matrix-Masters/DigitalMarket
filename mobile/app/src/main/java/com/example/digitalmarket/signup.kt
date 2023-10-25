ae1008d2185c5c67dc0923dd15d8af8fcec6b584
TalelMejri <talelmejri8@gmail.com>
2023-10-25 10:46:16 +0100

    add signup activity

---
+++ mobile/app/src/main/java/com/example/digitalmarket/signup.kt
@@ -0,0 +1,11 @@
+package com.example.digitalmarket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class signup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
