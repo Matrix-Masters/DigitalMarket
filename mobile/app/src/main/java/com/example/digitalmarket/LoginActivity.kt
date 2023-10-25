671caad13e30b43ff89c0da2675470067a307e0c
hkimi amin <hkimiamin02@gmail.com>
2023-10-25 20:26:16 +0100

    inout validation

--- mobile/app/src/main/java/com/example/digitalmarket/LoginActivity.kt
+++ mobile/app/src/main/java/com/example/digitalmarket/LoginActivity.kt
@@ -1,11 +1,31 @@
 package com.example.digitalmarket
-
+import android.view.View
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity(){
+    lateinit var Loginbutton: Button;
+    lateinit var Email:EditText;
+    lateinit var Password:EditText;
+    lateinit var errorText:TextView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+        Loginbutton=findViewById(R.id.loginButton)
+        Email=findViewById(R.id.email)
+        Password=findViewById(R.id.password)
+        errorText=findViewById(R.id.error)
+        Loginbutton.setOnClickListener(){
+        validForm();
+        }
+    }
+    public fun validForm(){
+    if(Password.text.length==0){
+        errorText.setText("fill out all the form fields!")
+        errorText.visibility=View.VISIBLE;
+    }
     }
 }
