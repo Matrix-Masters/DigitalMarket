e226ac3c5e932d437c461d8cc7ec4491ae44315e
ghassen <ghassenahmed74@gmail.com>
2023-10-26 23:33:05 +0100

    add annotation enableFeignClients

--- SprignBoot/InfoUser-service/src/main/java/org/dsi/InfoUserServiceApplication.java
+++ SprignBoot/InfoUser-service/src/main/java/org/dsi/InfoUserServiceApplication.java
@@ -2,8 +2,11 @@ package org.dsi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
 public class InfoUserServiceApplication {
 
 	public static void main(String[] args) {
