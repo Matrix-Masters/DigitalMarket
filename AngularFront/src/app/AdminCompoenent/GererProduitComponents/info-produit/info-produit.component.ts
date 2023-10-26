45e4e001b2ea7169c0d3cb5f6cd5c2b56ad2589a
hkimi amin <hkimiamin02@gmail.com>
2023-10-26 23:27:16 +0100

    update info produit

--- AngularFront/src/app/AdminCompoenent/GererProduitComponents/info-produit/info-produit.component.ts
+++ AngularFront/src/app/AdminCompoenent/GererProduitComponents/info-produit/info-produit.component.ts
@@ -1,4 +1,4 @@
-import { Component,EventEmitter,Inject, Input, Output  } from '@angular/core';
+import { Component,ElementRef,EventEmitter,Inject, Input, Output, ViewChild  } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AdminServiceService } from 'src/app/Service/admin-service.service';
 @Component({
@@ -11,15 +11,39 @@ export class InfoProduitComponent {
   @Input() visible: boolean = false;
   @Input() Product : any=null;
   @Output() hide = new EventEmitter<string>();
+  @Output() updateProducts = new EventEmitter<string>();
+  @ViewChild('closeBtn') close!: ElementRef;
   hideModal() {
     this.hide.emit('hide');
   }
+
+  UpdateProducts() {
+    this.updateProducts.emit('update products');
+  }
   AcceptProduit(id:number){
-    this.AdminServiceService.AcceptProduit(id).subscribe(res=>{
-        console.log(res);
-    })
+    this.AdminServiceService.AcceptProduit(id).subscribe((res: any)=>{
+      this.close.nativeElement.click();
+      this._snackBar.open("product accepted", 'close', {
+        duration: 3000
+      })
+
+    }),(error:any)=>{
+      this._snackBar.open("there was a problem with your action", 'close', {
+        duration: 3000
+      })
+    }
   }
   RefuseProduct(id:number){
+    this.AdminServiceService.RefuseProduct(id).subscribe((res: any)=>{
+      this.close.nativeElement.click();
+      this._snackBar.open("product Refused", 'close', {
+        duration: 3000
+      })
+    }),(error:any)=>{
+      this._snackBar.open("there was a problem with your action", 'close', {
+        duration: 3000
+      })
+    }
   }
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action);
