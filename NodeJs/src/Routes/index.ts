c909b6e01e257048bb0a087a22d6d91b18339e84
TalelMejri <talelmejri8@gmail.com>
2023-10-26 20:56:49 +0100

    add InfoUser Model

--- NodeJs/src/Routes/index.ts
+++ NodeJs/src/Routes/index.ts
@@ -4,5 +4,5 @@ const router=express.Router();
 // passer le router comme parametres
 require("./ProductsRoutes")(router);
 require("./CategoryRoutes")(router);
-
+require("./UserRoutes")(router);
 export default router;
