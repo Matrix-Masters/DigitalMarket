798476cad3cd423197ec88146b78e379060fe7b3
ghassen <ghassenahmed74@gmail.com>
2023-10-26 23:31:30 +0100

    add queries userInfo

--- SprignBoot/InfoUser-service/src/main/java/org/dsi/repo/UserRepo.java
+++ SprignBoot/InfoUser-service/src/main/java/org/dsi/repo/UserRepo.java
@@ -2,10 +2,17 @@ package org.dsi.repo;
 
 import org.dsi.entity.InfoUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
 public interface UserRepo extends JpaRepository<InfoUser,Long> {
+	
+	@Query(value="select * from info_user where email=:email",nativeQuery=true)
+	InfoUser getUserByemail(String email);
+	
+	@Query(value="select * from info_user where id=:id",nativeQuery=true)
+	InfoUser getUserById(Long id);
 
 }
