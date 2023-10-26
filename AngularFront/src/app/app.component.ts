8894fba18552165cab1e79426be34df1b8cfb477
TalelMejri <talelmejri8@gmail.com>
2023-10-26 23:06:23 +0100

    fix Active NavItem

--- AngularFront/src/app/app.component.ts
+++ AngularFront/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import Aos from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements  OnInit {
+
   title = 'AngularFront';
+
+  ngOnInit(): void {
+    Aos.init();
+  }
+
 }
