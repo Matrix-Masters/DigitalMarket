f1af740c14ccfe7bf5d9a342d6b9c6d1ec9fa52e
TalelMejri <talelmejri8@gmail.com>
2023-10-27 14:39:11 +0100

    add logo

--- AngularFront/src/app/Layout/side-bar-comp/side-bar-comp.component.ts
+++ AngularFront/src/app/Layout/side-bar-comp/side-bar-comp.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-side-bar-comp',
   templateUrl: './side-bar-comp.component.html',
