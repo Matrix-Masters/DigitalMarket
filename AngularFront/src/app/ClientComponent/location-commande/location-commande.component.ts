20f6d06d699ee77b0a434226b46ecef917d4bc04
TalelMejri <talelmejri8@gmail.com>
2023-10-30 23:02:20 +0100

    add marker for my location

--- AngularFront/src/app/ClientComponent/location-commande/location-commande.component.ts
+++ AngularFront/src/app/ClientComponent/location-commande/location-commande.component.ts
@@ -8,28 +8,44 @@ import * as L from 'leaflet';
 })
 export class LocationCommandeComponent implements OnInit  {
 
-  private map:any;
+  // private map:any;
 
-  constructor(){
-    this.map = L.Map;
-  }
+  // constructor(){
+  //   this.map = L.Map;
+  // }
 
-  private initMap(): void {
-    this.map = L.map('map', {
-      center: [51.505, -0.09],
-      zoom: 13
-    });
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 10,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-    tiles.addTo(this.map);
-  }
+  // private initMap(): void {
+  //   this.map = L.map('map', {
+  //     center: [ 39.8282, -98.5795 ],
+  //     zoom: 3
+  //   });
+
+  //   const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //     maxZoom: 18,
+  //     minZoom: 3,
+  //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  //   });
+
+  //   tiles.addTo(this.map);
+  // }
 
 
   ngOnInit(): void {
-    this.initMap();
-  }
+    navigator.geolocation.getCurrentPosition((position) => {
+    var map = L.map('map',{
+      zoom: 13
+    }).setView([position.coords.latitude,position.coords.longitude], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+ L.marker([position.coords.latitude,position.coords.longitude], { icon: L.icon({	iconUrl: '../../../assets/404-status-code-removebg-preview.png',  iconSize: [38, 95],}) }).addTo(map)
+    .bindPopup('Your Position.')
+    .openPopup();
+  })
+
+}
    
 }
