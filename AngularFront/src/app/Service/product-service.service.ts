35c4712d52886999acb7855bb646fbee785cd131
ghassen <ghassenahmed74@gmail.com>
2023-11-02 22:05:06 +0100

    add methods service product getProductsByCategoryId,getMAxPrice

--- AngularFront/src/app/Service/product-service.service.ts
+++ AngularFront/src/app/Service/product-service.service.ts
@@ -8,6 +8,13 @@ export class ProductServiceService{
   constructor(private http:HttpClient){
   }
   getProductNewArrivals(){
-    return this.http.get(`${port}/PRODUCT-SERVICE/products/getProductsNewArrivals`)
+    return this.http.get(`${port}/PRODUCT-SERVICE/products/getProductsNewArrivals`);
+  }
+  getProductsByCategoryId(id:any,page:number,pageSize:number,search:String,prix:number){
+    return this.http.get(`${port}/PRODUCT-SERVICE/products/ProductsByIdCategoriePaginate?id=${id}&page=${page}&per_page=${pageSize}&search=${search}&prix=${prix}`);
+  }
+
+  getMAxPrice(){
+    return this.http.get(`${port}/PRODUCT-SERVICE/products/getMaxPrice`);
   }
 }
