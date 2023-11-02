ffb8b01aec682ae0b5a0036127db5e46ff5d11cc
hkimi amin <hkimiamin02@gmail.com>
2023-11-02 01:50:08 +0100

    gernearta avatar

---
+++ AngularFront/src/app/Service/gravatar.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import * as md5 from 'md5';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GravatarService {
+  private readonly baseUrl = 'https://www.gravatar.com/avatar/';
+
+  constructor() {}
+
+  getGravatarUrl(email: string, size: number = 200, defaultImage: string): string {
+    const hash = md5(email.trim().toLowerCase());
+    const params = new URLSearchParams();
+    params.set('s', String(size));
+    params.set('d', defaultImage);
+    // Add more parameters as needed, such as rating and forcedefault
+
+    return `${this.baseUrl}${hash}?${params.toString()}`;
+}
+}
