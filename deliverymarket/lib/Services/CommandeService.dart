2ac1b2e608f5f7fae6f491aacc601130ad324ace
TalelMejri <talelmejri8@gmail.com>
2023-11-03 22:55:14 +0100

    GetInfoProduct

--- deliverymarket/lib/Services/CommandeService.dart
+++ deliverymarket/lib/Services/CommandeService.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:deliverymarket/Models/Commande.dart';
+import 'package:deliverymarket/Models/Product.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -8,6 +9,11 @@ import 'package:deliverymarket/Models/User.dart';
 class CommandeService{
 
   List<Commande> Commandes=[];
+
+  Product product=Product(Name: "", image: "", id: -1);
+
+  List<Product> products=[];
+
   String url="http://10.0.2.2:8888"; 
 
   Future<List<Commande>> GetCommandes() async{
@@ -29,5 +35,22 @@ class CommandeService{
       return [];
     }
   }
+
+Future<Product> GetInfoProduct(int id) async {
+  try {
+    final response = await http.get(Uri.parse("$url/PRODUCT-SERVICE/products/GetDetailsProd?id=" + id.toString()));
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
+      product=Product(Name: jsonData["name"], image: jsonData["image"], id: jsonData["id"]);
+      products.add(product);
+      return product;
+    } else {
+     return Product(Name: "", image: "", id: -1);
+    }
+  } catch (e) {
+     return Product(Name: "", image: "", id: -1);
+  }
+}
+
 
 }
