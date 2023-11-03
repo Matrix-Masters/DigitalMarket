af75b5ae20b28babe487f914747face564cea825
ghassen <ghassenahmed74@gmail.com>
2023-11-03 23:45:07 +0100

    add refresh page  + fix design

--- AngularFront/src/app/AccueilClient/accueil-cart/accueil-cart.component.ts
+++ AngularFront/src/app/AccueilClient/accueil-cart/accueil-cart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsServiceLocalStorageService } from 'src/app/Service/products-service-local-storage.service';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-accueil-cart',
   templateUrl: './accueil-cart.component.html',
@@ -8,7 +8,7 @@ import { ProductsServiceLocalStorageService } from 'src/app/Service/products-ser
 })
 export class AccueilCartComponent implements OnInit{
 
-  constructor(public productServiceStorage:ProductsServiceLocalStorageService){}
+  constructor(public productServiceStorage:ProductsServiceLocalStorageService,private location: Location){}
 products:any
 
 
@@ -17,6 +17,12 @@ getListProducts(){
   if(productString) {
     this.products = JSON.parse(productString);
   }
+}
+
+clearAll(){
+  this.productServiceStorage.clearProductList();
+  this.location.replaceState('/cart');
+  location.reload();
 }
 
   ngOnInit(): void {
