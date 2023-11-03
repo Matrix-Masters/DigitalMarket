48ac3855f524a441d2aa7878c6af409a2e013639
TalelMejri <talelmejri8@gmail.com>
2023-11-03 11:23:46 +0100

    CommandeDispo

---
+++ deliverymarket/lib/Screens/CommandeDispo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CommandeDispo extends StatefulWidget {
+  const CommandeDispo({super.key});
+
+  @override
+  State<CommandeDispo> createState() => _CommandeDispoState();
+}
+
+class _CommandeDispoState extends State<CommandeDispo> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("test");
+  }
+}
