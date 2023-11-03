a2751488d1825b04fad1be95e17a9057e00f873a
TalelMejri <talelmejri8@gmail.com>
2023-11-03 12:41:28 +0100

    Fix Relation , getComamndDisponible

--- GestionCommande/src/Controller/CommandeController.ts
+++ GestionCommande/src/Controller/CommandeController.ts
@@ -4,10 +4,23 @@ import LigneCommande from "../Model/LigneCommande";
 
 export const addCommande = async (req: Request, res: Response) => {
     try {
-        var prixTotal=0;
+        var prixTotal = 0;
+        const ligneCommandes = [];
+
         for (let i = 0; i < req.body.LigneCommandes.length; i++) {
-            prixTotal+=req.body.LigneCommandes[i].prix*req.body.LigneCommandes[i].Quantity;
+            prixTotal += req.body.LigneCommandes[i].prix * req.body.LigneCommandes[i].Quantity;
+
+     
+            const ligneCommande = new LigneCommande({
+                Commande_id: null, 
+                Product_id: req.body.LigneCommandes[i].Product_id,
+                Quantity: req.body.LigneCommandes[i].Quantity,
+                prix: req.body.LigneCommandes[i].prix,
+            });
+            const savedLigneCommande = await ligneCommande.save();
+            ligneCommandes.push(savedLigneCommande._id);
         }
+
         const commande = new Commande({
             NumCommande: req.body.NumCommande,
             Name: req.body.Name,
@@ -19,22 +32,30 @@ export const addCommande = async (req: Request, res: Response) => {
             Client_id: req.body.Client_id,
             location: {
                 latitude: req.body.location.latitude,
-                longitude: req.body.location.longitude
-            }
+                longitude: req.body.location.longitude,
+            },
+            LigneCommandes: ligneCommandes,
         });
-        const commandeSaved=await commande.save();
-        for (let i = 0; i < req.body.LigneCommandes.length; i++) {
-            const ligneCommande = new LigneCommande({
-                Commande_id: commandeSaved._id,
-                Product_id: req.body.LigneCommandes[i].Product_id,
-                Quantity: req.body.LigneCommandes[i].Quantity,
-                prix: req.body.LigneCommandes[i].prix,
-            });
-            await ligneCommande.save();
+
+        const commandeSaved = await commande.save();
+        // test worked :) add commande for chaque commande null
+        for (let i = 0; i < ligneCommandes.length; i++) {
+            await LigneCommande.findByIdAndUpdate(ligneCommandes[i], { Commande_id: commandeSaved._id });
         }
         res.status(201).json(commandeSaved);
     } catch (e) {
         res.status(400).send(e);
     }
+}
 
+export const GetCommandeDispo = async (req: Request, res: Response) => {
+    try {
+        //Make Relation 
+        const commandes = await Commande.find({
+            Status: "Disponible",
+        }).populate('LigneCommandes').exec();
+        res.status(200).json(commandes);
+    } catch (e) {
+        res.status(400).send(e);
+    }
 }
