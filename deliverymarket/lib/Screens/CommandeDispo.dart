55aabef200c1d28a8ec14b0157bef858d4b5ef26
TalelMejri <talelmejri8@gmail.com>
2023-11-03 18:57:25 +0100

    AfficherLisrComande

--- deliverymarket/lib/Screens/CommandeDispo.dart
+++ deliverymarket/lib/Screens/CommandeDispo.dart
@@ -1,3 +1,5 @@
+import 'package:deliverymarket/Models/Commande.dart';
+import 'package:deliverymarket/Services/CommandeService.dart';
 import 'package:flutter/material.dart';
 
 class CommandeDispo extends StatefulWidget {
@@ -8,12 +10,45 @@ class CommandeDispo extends StatefulWidget {
 }
 
 class _CommandeDispoState extends State<CommandeDispo> {
+
+
+   late CommandeService commandeService=CommandeService();
+   List<Commande> Commandes=[];
+
+  @override
+  void initState() { 
+    super.initState();
+    this.fetchCommandes();
+  }
+
+ Future<void> fetchCommandes()async {
+    Commandes=[];
+    await commandeService.GetCommandes();
+    setState(() {
+      Commandes=commandeService.Commandes;
+    });
+ }
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Commande Disponible"),
-      ),
+    return  Scaffold(
+      body:Column(
+         children: [
+           Expanded(child: 
+             ListView.builder(
+              itemCount:Commandes.length,
+              itemBuilder: (context,index){
+                  final commande=Commandes[index];
+                  return Card(
+                      child: ListTile(
+                        leading: Text(commande.NumCommande),
+                        title: Text(commande.location['latitude'].toString()),
+                        subtitle: Text(commande.PrixTotal),
+                        trailing: Text(commande.Status),
+                      ),
+                  );
+             }))
+         ],
+      )
     );
   }
 }
