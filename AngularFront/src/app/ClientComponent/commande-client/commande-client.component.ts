570fd556c987cfa032d3df53548dd1cfbff65241
TalelMejri <talelmejri8@gmail.com>
2023-11-17 13:59:38 +0100

    make livreur move in map

--- AngularFront/src/app/ClientComponent/commande-client/commande-client.component.ts
+++ AngularFront/src/app/ClientComponent/commande-client/commande-client.component.ts
@@ -17,12 +17,9 @@ export class CommandeClientComponent  implements OnInit  {
 
   map :any;
   InitMap(){
-    navigator.geolocation.getCurrentPosition((position) => {
-
       if (this.map) {
         this.map.remove();
       }
-
         this.map = L.map('map',{
             zoom: 13
         }).setView([this.LocationForClient.lat,this.LocationForClient.log], 13);
@@ -37,22 +34,20 @@ export class CommandeClientComponent  implements OnInit  {
         subdomains:['mt0','mt1','mt2','mt3']
       }).addTo(this.map);
     
-
      var baseMaps = {
       "Google Street": googleSreet,
       "OpenStreetMap": openstreetmap,
      }
   
-
      L.control.layers(baseMaps).addTo(this.map);
 
-     L.marker([position.coords.latitude, position.coords.longitude], {
+     var marker1=L.marker([this.LivreurLocation.lat,this.LivreurLocation.log], {
       icon: L.icon({ iconUrl: '../../../assets/Develery.gif', iconSize: [100, 100] }),
       zIndexOffset: 1000  
     })
     .addTo(this.map);
     
-    L.marker([this.LocationForClient.lat, this.LocationForClient.log], {
+   L.marker([this.LocationForClient.lat,this.LocationForClient.log], {
       icon: L.icon({ iconUrl: '../../../assets/location.svg', iconSize: [30, 40] }),
       zIndexOffset: 1000 
     })
@@ -62,7 +57,7 @@ export class CommandeClientComponent  implements OnInit  {
 
      L.Routing.control({
       waypoints: [
-        L.latLng(position.coords.latitude,position.coords.longitude),
+        L.latLng(this.LivreurLocation.lat,this.LivreurLocation.log),
         L.latLng(this.LocationForClient.lat,this.LocationForClient.log)
       ],
       lineOptions:{
@@ -80,14 +75,27 @@ export class CommandeClientComponent  implements OnInit  {
       showAlternatives:false,
       fitSelectedRoutes:true,
       addWaypoints:false,
-    }).addTo(this.map);
+    }).on("routesfound",function(e){
+      e.routes[0].coordinates.forEach((c:any,i:any)=>{
+        setTimeout(()=>{
+          marker1.setLatLng([
+            c.lat,c.lng
+          ]);
+        },1000*i);
+      })
+    })
+    .addTo(this.map);
  
-  })
   }
 
   SelectCommande:any=null;
   Listcommandes:any;
   LocationForClient={
+    lat:0,
+    log:0
+  }
+
+  LivreurLocation={
     lat:0,
     log:0
   }
@@ -102,10 +110,18 @@ export class CommandeClientComponent  implements OnInit  {
     this.SelectCommande=null;
     this.LocationForClient.lat=0;
     this.LocationForClient.log=0;
-    this.SelectCommande=this.Listcommandes.find((v:any)=>v.NumCommande==data);
+    this.SelectCommande=data
     this.LocationForClient.lat=this.SelectCommande.location.latitude;
     this.LocationForClient.log=this.SelectCommande.location.longitude;
-    this.InitMap();
+    this.GetLocationForLivreaurr();
+  }
+
+  GetLocationForLivreaurr(){
+    this.CommandeServiceService.GetLivraisonByNumCommande(this.SelectCommande.NumCommande).subscribe((res:any)=>{
+        this.LivreurLocation.lat=res.Location.latitude;
+        this.LivreurLocation.log=res.Location.longitude;
+        this.InitMap();
+    })
   }
 
   ngOnInit(): void {
