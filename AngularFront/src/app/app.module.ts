249b6f2b1f8dd20bd0ea8d1a631443926894320f
ghassen <ghassenahmed74@gmail.com>
2023-11-17 22:50:14 +0100

    import component

--- AngularFront/src/app/app.module.ts
+++ AngularFront/src/app/app.module.ts
@@ -34,6 +34,11 @@ import { CardCartComponent } from './AccueilClient/card-cart/card-cart.component
 
 import { NotFoundComponentComponent } from './notfound/not-found-component/not-found-component.component';
 import { CommandeComponent } from './ClientComponent/commande/commande.component';
+import { FournisseurDashboardComponent } from './Fournissuer/fournisseur-dashboard/fournisseur-dashboard.component';
+import { FournisseurInterfaceComponent } from './Fournissuer/fournisseur-interface/fournisseur-interface.component';
+import { FournisseurNavbarCompComponent } from './Fournissuer/fournisseur-navbar-comp/fournisseur-navbar-comp.component';
+import { FournisseurSideBarCompComponent } from './Fournissuer/fournisseur-side-bar-comp/fournisseur-side-bar-comp.component';
+import { AddProductComponent } from './Fournissuer/add-product/add-product.component';
 
 
 @NgModule({
@@ -61,7 +66,12 @@ import { CommandeComponent } from './ClientComponent/commande/commande.component
     NotFoundComponentComponent,
     CommandeComponent,
     GererSupplierComponent,
-    LocationCommandeComponent
+    LocationCommandeComponent,
+    FournisseurDashboardComponent,
+    FournisseurInterfaceComponent,
+    FournisseurNavbarCompComponent,
+    FournisseurSideBarCompComponent,
+    AddProductComponent
 
   ],
   imports: [
