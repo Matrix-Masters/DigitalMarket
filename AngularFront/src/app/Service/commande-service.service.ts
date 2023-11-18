00647ae0f24ab2dc6ff9a14593d8aaea5138ebf4
Youssef Souissi <souissiyoussef9@gmail.com>
2023-11-18 20:49:29 +0100

    add DetailsProd ,getCommandes functions in service

--- AngularFront/src/app/Service/commande-service.service.ts
+++ AngularFront/src/app/Service/commande-service.service.ts
@@ -11,5 +11,15 @@ export class CommandeServiceService {
   AddCommande(data:any){
     return this.http.post(`${port}/GESTIONCOMMANDE-SERVICE/Commande/AddCommande`,data);
   }
+  DetailsProd(id:number){
+    return this.http.get(`${port}/PRODUCT-SERVICE/products/GetDetailsProd?id=`+id);
+  }
+  getCommandes(page: number, size: number, search: string)
+  {
+    
+    return this.http.get(`${port}/GESTIONCOMMANDE-SERVICE/commande/GetCommandsWaiting?page=${page}&size=${size}&${search!='' ? 'search='+search : ''}`);
+  }
+
 
 }
+
