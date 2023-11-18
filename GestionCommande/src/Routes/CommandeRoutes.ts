346c027d3682ccd9e3a8ac2c374c608bc4b7ef3c
Youssef Souissi <souissiyoussef9@gmail.com>
2023-11-18 20:46:23 +0100

    add commandswaiting req in commandeRoutes

--- GestionCommande/src/Routes/CommandeRoutes.ts
+++ GestionCommande/src/Routes/CommandeRoutes.ts
@@ -1,7 +1,9 @@
-import { addCommande,GetCommandeDispo, AddLivraison} from "../Controller/CommandeController"
+import { addCommande,GetCommandeDispo, AddLivraison, AcceptCommand,GetCommandsWaiting} from "../Controller/CommandeController"
 
 module.exports=(router:any)=>{
     router.post('/AddCommande',addCommande);
     router.get('/GetCommandeDispo',GetCommandeDispo);
     router.post('/AddLivraison',AddLivraison);
+    router.get('/AcceptCommand/:num',AcceptCommand);
+    router.get('/GetCommandsWaiting',GetCommandsWaiting);
 }
