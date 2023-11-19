94a0b718920074af8b06c07691b9431ac0a48440
Youssef Souissi <souissiyoussef9@gmail.com>
2023-11-19 18:04:34 +0100

    add refused and delete commande in routes

--- GestionCommande/src/Routes/CommandeRoutes.ts
+++ GestionCommande/src/Routes/CommandeRoutes.ts
@@ -1,9 +1,12 @@
-import { addCommande,GetCommandeDispo, AddLivraison, AcceptCommand,GetCommandsWaiting} from "../Controller/CommandeController"
+import { addCommande,GetCommandeDispo, AddLivraison, AcceptCommand,GetCommandsWaiting,RefusedCommand,deleteCommande} from "../Controller/CommandeController"
 
 module.exports=(router:any)=>{
     router.post('/AddCommande',addCommande);
     router.get('/GetCommandeDispo',GetCommandeDispo);
     router.post('/AddLivraison',AddLivraison);
-    router.get('/AcceptCommand/:num',AcceptCommand);
+    router.put('/AcceptCommand/:num',AcceptCommand);
+    router.put('/RefusedCommand/:num',RefusedCommand);
     router.get('/GetCommandsWaiting',GetCommandsWaiting);
+    router.delete('/DeleteCommande/:id', deleteCommande);
+
 }
