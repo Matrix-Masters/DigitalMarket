d1520f287d651838652b5e6c0bec23a06db8b1ae
Youssef Souissi <souissiyoussef9@gmail.com>
2023-11-19 18:09:27 +0100

    modify toggleFavorite function to add, remove prod

--- AngularFront/src/app/home/card/card.component.ts
+++ AngularFront/src/app/home/card/card.component.ts
@@ -1,31 +1,61 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ProductsServiceLocalStorageService } from 'src/app/Service/products-service-local-storage.service';
-import { Product } from "../../Model/Product";
+import { Product } from '../../Model/Product';
+import { WishlistService } from 'src/app/Service/wishlist.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
-export class CardComponent  implements OnInit{
-isFavorite:boolean = false;
-constructor(public productServiceStorage:ProductsServiceLocalStorageService){}
-@Input() products:any
-@Input() new:boolean = true;
+export class CardComponent implements OnInit {
+  isFavorite: boolean = false;
+  constructor(
+    public productServiceStorage: ProductsServiceLocalStorageService,
+    private wishlistService: WishlistService,
+    private snackBar: MatSnackBar
+  ) {}
+  @Input() products: any;
+  @Input() new: boolean = true;
 
-  toggleFavorite():void{
-    this.isFavorite = !this.isFavorite;
+  toggleFavorite(id: any): void {
+    this.wishlistService
+      .addToWishlist({
+        idUser: 2,
+        idProduct: id,
+      })
+      .subscribe(
+        (response: any) => {
+          console.log('Added to wishlist successfully', response);
+          this.snackBar.open(response.message, 'Close', {
+            duration: 3000,
+          });
+
+          this.isFavorite = !this.isFavorite;
+        },
+        (error: any) => {
+          console.error('Error adding to wishlist', error);
+
+          this.isFavorite = !this.isFavorite;
+
+          this.snackBar.open(
+            'Failed to add the Product to your wishlist',
+            'Close',
+            {
+              duration: 3000,
+            }
+          );
+        }
+      );
   }
 
-  addProduct(product :any) {
+  addProduct(product: any) {
     this.productServiceStorage.addProduct(product);
   }
 
-
-  clear(){
+  clear() {
     this.productServiceStorage.clearProductList();
   }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 }
