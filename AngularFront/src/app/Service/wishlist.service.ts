6916a3a2d956ee7db87c9bdd37deb93b8c6f7f02
Youssef Souissi <souissiyoussef9@gmail.com>
2023-11-19 17:58:51 +0100

    create new service : wishlist service

---
+++ AngularFront/src/app/Service/wishlist.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { port } from 'src/env';
+@Injectable({
+  providedIn: 'root'
+})
+export class WishlistService {
+
+  constructor(private http:HttpClient) { }
+
+ 
+
+}
