aba6f8f32237af1a59e402ded85467c18ffba3f8
ghassen <ghassenahmed74@gmail.com>
2023-11-21 19:11:13 +0100

    add navbar and footer

--- AngularFront/src/app/EditProfile/edit-profile/edit-profile.component.ts
+++ AngularFront/src/app/EditProfile/edit-profile/edit-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthServiceService } from 'src/app/Service/auth-service.service';
+import { CategoryServiceService } from 'src/app/Service/category-service.service';
 
 @Component({
   selector: 'app-edit-profile',
@@ -7,4 +10,24 @@ import { Component } from '@angular/core';
 })
 export class EditProfileComponent {
 
+  categories:any;
+  id:any
+
+
+
+
+  constructor(public AuthServiceService:AuthServiceService, public categoriesService : CategoryServiceService,private route: ActivatedRoute , private router: Router){}
+
+
+
+  ngOnInit():void {
+    this.categoriesService.getAllCategories().subscribe(
+      (res:any)=>{
+        this.categories=res;
+      },
+      err=>{
+        console.log(err);
+      }
+    )
+  }
 }
