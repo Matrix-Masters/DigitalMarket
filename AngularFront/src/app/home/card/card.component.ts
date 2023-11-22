8889b432c8a1785dd9072f7334466b5a2095edc7
ghassen <ghassenahmed74@gmail.com>
2023-11-22 20:15:11 +0100

    add snackbar add product

--- AngularFront/src/app/home/card/card.component.ts
+++ AngularFront/src/app/home/card/card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ProductsServiceLocalStorageService } from 'src/app/Service/products-service-local-storage.service';
 import { Product } from "../../Model/Product";
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -8,7 +9,7 @@ import { Product } from "../../Model/Product";
 })
 export class CardComponent  implements OnInit{
 isFavorite:boolean = false;
-constructor(public productServiceStorage:ProductsServiceLocalStorageService){}
+constructor(public productServiceStorage:ProductsServiceLocalStorageService, private MatSnackBar:MatSnackBar){}
 @Input() products:any
 @Input() new:boolean = true;
 
@@ -18,6 +19,9 @@ constructor(public productServiceStorage:ProductsServiceLocalStorageService){}
 
   addProduct(product :any) {
     this.productServiceStorage.addProduct(product);
+    this.MatSnackBar.open("Product Added",'',{
+      duration:2000,
+    })
   }
 
 
