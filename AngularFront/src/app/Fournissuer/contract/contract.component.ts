2a781e5dbcfa82990a25d4e799802baff3371c2b
TalelMejri <talelmejri8@gmail.com>
2023-11-23 00:32:10 +0100

    crud local sur products and makeContract

--- AngularFront/src/app/Fournissuer/contract/contract.component.ts
+++ AngularFront/src/app/Fournissuer/contract/contract.component.ts
@@ -3,10 +3,8 @@ import * as p5 from 'p5';
 import { jsPDF } from "jspdf";
 import autoTable from 'jspdf-autotable'
 import { ContractServiceService } from 'src/app/Service/contract-service.service';
-
-
-
-
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-contract',
   templateUrl: './contract.component.html',
@@ -15,12 +13,16 @@ import { ContractServiceService } from 'src/app/Service/contract-service.service
 
 export class ContractComponent implements OnInit {
 
-  constructor(private ContractServiceService:ContractServiceService) { }
+  constructor(private ContractServiceService:ContractServiceService,private MatSnackBar:MatSnackBar,private datePipe: DatePipe) { }
   canvas: any;
   c: p5.Color[] = [];
 
   ngOnInit(): void {
+    this.InitSignature();
+  }
 
+
+  InitSignature(){
     const sketch = (s: p5) => {
       s.setup = () => {
         let canvas2 = s.createCanvas(s.windowWidth - 1400, s.windowHeight - 600);
@@ -42,19 +44,16 @@ export class ContractComponent implements OnInit {
     this.canvas = new p5(sketch);
   }
 
-  products=[
-      "accesoire","tv"
-  ]
 
+  
   image:string="";
-
+ 
   captureSignature() {
     
     const signatureDataUrl = this.canvas.canvas.toDataURL();
     this.canvas.clear();
    // this.canvas.isEmpty();
     this.image=signatureDataUrl
-    console.log(signatureDataUrl);
     const doc = new jsPDF();
     doc.addImage(signatureDataUrl, 2, 2, 20, 20);
     doc.addImage('../../../assets/DIGITAL MARKET logo.png','png', 2, 2, 20, 20);
@@ -79,9 +78,9 @@ export class ContractComponent implements OnInit {
     doc.text('Prénom :',12,90);
     doc.text('Email :',12,95);
     doc.setFont("times", "normal");
-    doc.text("Talel",24,85);
-    doc.text("Mejri",30,90);
-    doc.text("talel@gmail.com",26,95);
+    doc.text(`${this.name}`,24,85);
+    doc.text(`${this.last}`,30,90);
+    doc.text(`${this.email}`,26,95);
     doc.setFontSize(15);
     doc.setFont("times", "bold");
     doc.text("Article 1: PURPOSE OF THE CONTRACT",12,110);
@@ -95,12 +94,12 @@ export class ContractComponent implements OnInit {
     doc.text("Article 2: The Contractors During",12,140);
     doc.setFont("times", "normal");
     doc.setFontSize(14);
-    doc.text("This contract is Start from 12/09/2002 to 15/06/2002",15,148);
+    doc.text(`This contract is Start from ${new Date().toString()} to ${this.date}`,15,148);
     doc.text("Signature",158,doc.internal.pageSize.height - 50);
     doc.addImage(this.image, 158, doc.internal.pageSize.height - 47, 20, 20);
 
     const header = ['Name Product'];
-    const body = this.products.map(product => [product]);
+    const body = this.products.map((product:any) => [product]);
     doc.setFont("times", "bold");
     doc.setFontSize(15);
     doc.text("Article 3: Your Products",12,160);
@@ -115,19 +114,61 @@ export class ContractComponent implements OnInit {
     doc.setFontSize(12);
     doc.text('If you have any questions about Contract, do not hesitate to contact us' ,45, doc.internal.pageSize.height - 20);
     doc.text('1/1',105, doc.internal.pageSize.height - 10);
-    doc.save(`Contract.pdf`);   
+    var nameContract:string=`${Math.random()+this.name}.pdf`;
+    doc.save(`${nameContract}`);   
     const pdfBase64 = doc.output('datauristring');
-    const pdfFile = this.base64ToFile(pdfBase64, 'Contract.pdf', 'application/pdf');
+    const pdfFile = this.base64ToFile(pdfBase64, `${nameContract}`, 'application/pdf');
+    console.log(this.datePipe.transform(this.date, 'yyyy-MM-dd HH:mm:ss'));
     this.ContractServiceService.AddConract(
           pdfFile,
-          "dd",
-          new Date(),
-          [],
+          nameContract,
+          new Date(this.date),
+          this.products,
           1
     ).subscribe((res:any)=>{
-       
+        this.MatSnackBar.open("Contract added successfully","Close",
+          {
+            duration: 2000,
+            verticalPosition: 'bottom',
+            panelClass: ['red-snackbar']
+          }
+        );
     })
 
+  }
+  products: string[] = [];
+  Product:string="";
+  clearSignature(){
+    this.canvas.clear();
+    //this.InitSignature();
+  }
+
+  deleteProduct(product:string){
+    this.products = this.products.filter((v:any)=>v!=product);
+  }
+
+  AddProduct(){
+    if(this.products.includes(this.Product)){
+      alert("This product is already exist");
+      return;
+    }else if(this.Product==""){
+      alert("Please fill the product name");
+      return;
+    }
+    this.products.push(this.Product);
+    this.Product="";
+  }
+  name="";
+  last="";
+  email="";
+  date="";
+
+  MakeContract(){
+      if(this.name!="" && this.last!="" && this.email!="" && this.date!="" && this.products.length>0){
+        this.captureSignature();
+      }else{
+        alert("Please fill all the fields");
+      }
   }
 
   base64ToFile(base64: string, filename: string, mimeType: string): File {
