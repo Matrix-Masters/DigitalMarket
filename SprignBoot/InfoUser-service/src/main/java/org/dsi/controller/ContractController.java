1df17c1e4b86253ee51fc6be3fee915f99ea4bea
TalelMejri <talelmejri8@gmail.com>
2023-11-23 20:50:09 +0100

    load file pdf

--- SprignBoot/InfoUser-service/src/main/java/org/dsi/controller/ContractController.java
+++ SprignBoot/InfoUser-service/src/main/java/org/dsi/controller/ContractController.java
@@ -4,12 +4,17 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.dsi.entity.Contract;
 import org.dsi.entity.Product;
 import org.dsi.payload.contractInfo;
+import org.dsi.repo.ContractRepo;
 import org.dsi.service.ContractService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +30,9 @@ public class ContractController {
 	
 	 @Autowired
 	 ContractService ContractService;
+	 
+	 @Autowired
+	 ContractRepo ContractRepo;
 	
 	@PostMapping
 	public ResponseEntity<?> AddContract(
@@ -43,5 +51,16 @@ public class ContractController {
 		}
 			
 	}
+	
+	  @GetMapping("/pdf/{contractId}")
+	    public ResponseEntity<?> serveFile(@PathVariable long contractId) {
+	        try {
+	        	Contract contract=ContractRepo.findById(contractId).get();
+	            Resource file = ContractService.loadFileAsResource(contract.getNameContract());
+	            return ResponseEntity.ok().body(file);
+	        } catch (Exception e) {
+	        	return new ResponseEntity<String>("Not Found",HttpStatus.NOT_FOUND);
+	        }
+	    }
 	
 }
