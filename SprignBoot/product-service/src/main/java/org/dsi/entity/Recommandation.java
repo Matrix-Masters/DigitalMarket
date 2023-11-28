e39566c406e034697e19d229c460cde7b8304748
ghassen <ghassenahmed74@gmail.com>
2023-11-28 20:55:45 +0100

    add count column

--- SprignBoot/product-service/src/main/java/org/dsi/entity/Recommandation.java
+++ SprignBoot/product-service/src/main/java/org/dsi/entity/Recommandation.java
@@ -8,6 +8,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.ColumnDefault;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,6 +26,9 @@ public class Recommandation {
 	private Long Id;
 	
 	private Long UserId;
+	
+	@ColumnDefault(value="0")
+	private Long count;
 	
 	@ManyToOne
 	@JoinColumn(name="product_id",nullable=false)
