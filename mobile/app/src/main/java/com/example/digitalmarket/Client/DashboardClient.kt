b55e71c0336055dba3c44a619b9ca4833af635e2
TalelMejri <talelmejri8@gmail.com>
2023-12-01 17:59:47 +0100

    got to panier

--- mobile/app/src/main/java/com/example/digitalmarket/Client/DashboardClient.kt
+++ mobile/app/src/main/java/com/example/digitalmarket/Client/DashboardClient.kt
@@ -1,16 +1,23 @@
 package com.example.digitalmarket.Client
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.digitalmarket.R
+import com.example.digitalmarket.listProduct.ListProduct
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class DashboardClient : AppCompatActivity() {
+    lateinit var go_back_img: ImageView
+    lateinit var menu_img: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard_client)
+        go_back_img = findViewById(R.id.go_back_img)
+        menu_img = findViewById(R.id.menu_img)
         replaceFragment(Panier())
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
@@ -20,7 +27,7 @@ class DashboardClient : AppCompatActivity() {
                     true
                 }
                 R.id.page_2->{
-                    replaceFragment(Settings())
+                    replaceFragment(ListProduct())
                     true
                 }
                 else -> false
