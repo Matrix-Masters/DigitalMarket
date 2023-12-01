244466461b9c2b3a565942e5e3a18dba023e7a4f
TalelMejri <talelmejri8@gmail.com>
2023-12-01 18:00:39 +0100

    PanierData

--- mobile/app/src/main/java/com/example/digitalmarket/Client/panier/PanierData.kt
+++ mobile/app/src/main/java/com/example/digitalmarket/Client/panier/PanierData.kt
@@ -1,4 +1,4 @@
 package com.example.digitalmarket.Client.panier
 
-data class PanierData(val nameProduct:String,val photo:Int,val qte:Int,val prix:Double ) {
+data class PanierData(val nameProduct:String, val photo:Int, var qte:Int, val prix:Double ) {
 }
