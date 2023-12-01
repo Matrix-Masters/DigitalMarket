4d244c2f875b36b95e444b94fb359aa4b8365b13
TalelMejri <talelmejri8@gmail.com>
2023-12-01 18:01:33 +0100

    changerColor

--- mobile/app/src/main/java/com/example/digitalmarket/listProduct/Product.kt
+++ mobile/app/src/main/java/com/example/digitalmarket/listProduct/Product.kt
@@ -3,4 +3,5 @@ package com.example.digitalmarket.listProduct
 import android.widget.ImageView
 
 data class Product (val name:String,val price:Double,val img_id:Int,var isSelected:Boolean=false) {
+
 }
