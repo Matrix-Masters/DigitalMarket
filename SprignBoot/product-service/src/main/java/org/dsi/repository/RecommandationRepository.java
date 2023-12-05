4f843d052fdd3096f97fa645cd9c92d45bb6d2f7
ghassen <ghassenahmed74@gmail.com>
2023-12-06 00:10:55 +0100

    add queries

--- SprignBoot/product-service/src/main/java/org/dsi/repository/RecommandationRepository.java
+++ SprignBoot/product-service/src/main/java/org/dsi/repository/RecommandationRepository.java
@@ -11,7 +11,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface RecommandationRepository extends JpaRepository<Recommandation, Long>{
 	
-	   @Query(value = "SELECT * FROM Recommandation where user_id = :id",nativeQuery=true)
+	   @Query(value = "SELECT * FROM Recommandation where user_id = :id order by count limit 6",nativeQuery=true)
 	   List<Recommandation> getRecommandationsById(Long id);
 	   
 	   @Query(value = "select * from recommandation order by count limit 6",nativeQuery=true)
