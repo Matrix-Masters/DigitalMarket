e25f628ae693557f72271410db8042e6ee0ee8a9
ghassen <ghassenahmed74@gmail.com>
2023-12-05 22:39:13 +0100

    add getRecommandation service

--- SprignBoot/product-service/src/main/java/org/dsi/service/RecommandationService.java
+++ SprignBoot/product-service/src/main/java/org/dsi/service/RecommandationService.java
@@ -1,5 +1,9 @@
 package org.dsi.service;
 
+import java.util.List;
+
+import org.dsi.entity.Product;
+import org.dsi.entity.Recommandation;
 import org.dsi.repository.RecommandationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +13,17 @@ public class RecommandationService {
 	
 	@Autowired
 	RecommandationRepository recommandationRepository;
+	
+	public List<Recommandation> getRecommandations(Long id) {
+
+	    List<Recommandation> defaultRecommandations = recommandationRepository.getDefaultRecommandations();
+	    List<Recommandation> recommandationsById = recommandationRepository.getRecommandationsById(id);
+
+	    if (recommandationsById.size() < 6) {
+	       return defaultRecommandations;  
+	    } else {
+	       return recommandationsById;
+	    }
+	}
 
 }
