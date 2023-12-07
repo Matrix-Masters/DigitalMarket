4c8522be12598705b75a9d8249067e9936be3336
ghassen <ghassenahmed74@gmail.com>
2023-12-07 21:47:06 +0100

    add addCount function service

--- SprignBoot/product-service/src/main/java/org/dsi/service/RecommandationService.java
+++ SprignBoot/product-service/src/main/java/org/dsi/service/RecommandationService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.dsi.entity.Product;
 import org.dsi.entity.Recommandation;
+import org.dsi.repository.ProductRepo;
 import org.dsi.repository.RecommandationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,9 @@ public class RecommandationService {
 	
 	@Autowired
 	RecommandationRepository recommandationRepository;
+	
+	@Autowired
+	ProductRepo productRepository;
 	
 	public List<Recommandation> getRecommandations(Long id) {
 
@@ -24,6 +28,28 @@ public class RecommandationService {
 	    } else {
 	       return recommandationsById;
 	    }
+	}
+	
+	
+	public void addCount(Long user_id,Long product_id,int count) {
+		Product product = productRepository.findProductById(product_id);
+		Recommandation recommandation = recommandationRepository.checkRecommandation(user_id, product_id);
+		
+		if(recommandation != null) {
+			
+			recommandation.setCount(recommandation.getCount()+count);
+			recommandationRepository.save(recommandation);
+			
+		}else {
+			
+			Recommandation newRecommandation = new Recommandation();
+			newRecommandation.setUser_id(user_id);
+			newRecommandation.setCount(count);
+			newRecommandation.setProduct(product);
+			recommandationRepository.save(newRecommandation);
+			
+		}
+		
 	}
 
 }
