a1962aa9e521f6a03aa544c974c24349abed2399
TalelMejri <talelmejri8@gmail.com>
2023-12-08 23:32:08 +0100

    User data

--- mobile/app/src/main/java/com/example/digitalmarket/Models/User.kt
+++ mobile/app/src/main/java/com/example/digitalmarket/Models/User.kt
@@ -1,4 +1,13 @@
 package com.example.digitalmarket.Models
 
-class User {
-}
+data class User (
+    val firstName : String? = null,
+    val lastName :String? = null,
+    val email :String? = null,
+    val password:String? = null,
+    val numTlf: String? = null,
+    val photoCin:String? = null,
+    val sexe: String? = null,
+    val photo:String? = null,
+    val role:String? = null,
+);
