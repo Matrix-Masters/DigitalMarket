7e8db9575b6c661fa67b0c42b6423e79016b8863
TalelMejri <talelmejri8@gmail.com>
2023-12-08 23:32:02 +0100

    ServiceBuilder

--- mobile/app/src/main/java/com/example/digitalmarket/api/ServiceBuilder.kt
+++ mobile/app/src/main/java/com/example/digitalmarket/api/ServiceBuilder.kt
@@ -1,4 +1,31 @@
 package com.example.digitalmarket.api
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ServiceBuilder {
+
+    private const val BASE_URL: String = "http://192.168.1.122:8888"
+
+    private val gson : Gson by lazy {
+        GsonBuilder().setLenient().create()
+    }
+    private val httpClient : OkHttpClient by lazy {
+        OkHttpClient.Builder().build()
+    }
+
+    private val retrofit : Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(httpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+
+    val apiService : RestApi by lazy{
+        retrofit.create(RestApi::class.java)
+    }
 }
