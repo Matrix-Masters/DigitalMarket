231d18c0e3883bd079b68f69a7e35726702a443f
TalelMejri <talelmejri8@gmail.com>
2023-12-08 23:31:58 +0100

    RestApi

--- mobile/app/src/main/java/com/example/digitalmarket/api/RestApi.kt
+++ mobile/app/src/main/java/com/example/digitalmarket/api/RestApi.kt
@@ -1,4 +1,13 @@
 package com.example.digitalmarket.api
+import com.example.digitalmarket.Models.User
+import okhttp3.ResponseBody
+import okhttp3.MultipartBody
+import retrofit2.http.*
 
 interface RestApi {
+    @POST("/INFOUSER-SERVICE/addUserInfo")
+    suspend fun CreateAnAccount(@Body User: User);
+    @Multipart
+    @POST("/PYTHON-SERVICE/api/upload")
+    suspend fun ExtractData(@Part image: MultipartBody.Part):ResponseBody
 }
