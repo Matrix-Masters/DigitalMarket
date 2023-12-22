528c2a58518ea12ecfbb6c58d8389dd9f5468290
TalelMejri <talelmejri8@gmail.com>
2023-12-22 01:21:16 +0100

    getImagesProducts in repository

--- SprignBoot/product-service/src/main/java/org/dsi/repository/ImageProduct.java
+++ SprignBoot/product-service/src/main/java/org/dsi/repository/ImageProduct.java
@@ -1,8 +1,17 @@
 package org.dsi.repository;
 
+import java.util.List;
+import org.springframework.data.repository.query.Param;
 import org.dsi.entity.ProductImages;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource
 public interface ImageProduct extends JpaRepository<ProductImages,Long> {
-
+	
+	@Query(value = "SELECT pi FROM ProductImages pi WHERE pi.product.id = :id order by pi.ImagePriorite")
+	List<ProductImages> getImagesProducts(@Param("id") long id);
+ 
 }
