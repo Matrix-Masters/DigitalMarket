5852a391a4352e0b6670809f983ceff302c4e975
ghassen <ghassenahmed74@gmail.com>
2023-12-23 16:17:40 +0100

    add reviewRoutes

--- GestionCommande/src/Routes/ReviewRoutes.ts
+++ GestionCommande/src/Routes/ReviewRoutes.ts
@@ -1,3 +1,5 @@
+import { addReview ,getReviews } from "../Controller/ReviewController";
 module.exports=(router:any)=>{
-    
+    router.post('/addReview',addReview);
+    router.get('/getReviews',getReviews);
 }
