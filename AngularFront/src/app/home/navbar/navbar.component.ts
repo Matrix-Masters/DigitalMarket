e183ea26ff31aa6f4f5aa780edb9f487fed17896
Youssef Souissi <souissiyoussef9@gmail.com>
2023-12-23 18:56:02 +0100

    receive notifications functions with socket io

--- AngularFront/src/app/home/navbar/navbar.component.ts
+++ AngularFront/src/app/home/navbar/navbar.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AuthServiceService } from 'src/app/Service/auth-service.service';
 import { CategoryServiceService } from 'src/app/Service/category-service.service';
 import { ProductsServiceLocalStorageService } from 'src/app/Service/products-service-local-storage.service';
+import { CommandeServiceService } from 'src/app/Service/commande-service.service';
+import { Websocket } from 'src/app/Service/websocket.service';
 
 
 @Component({
@@ -17,7 +19,7 @@ export class NavbarComponent implements OnInit {
   nbrArticle:number=0
 
 
-  constructor(public AuthServiceService:AuthServiceService, public categoriesService : CategoryServiceService){}
+  constructor(public AuthServiceService:AuthServiceService, public categoriesService : CategoryServiceService ,private commandeService: CommandeServiceService,private webSocketService: Websocket){}
 
 
 
@@ -32,6 +34,7 @@ export class NavbarComponent implements OnInit {
 
 
   ngOnInit():void {
+    
     this.categoriesService.getAllCategories().subscribe(
       (res:any)=>{
         this.categories=res;
@@ -45,10 +48,57 @@ export class NavbarComponent implements OnInit {
     for(let i = 0; i < this.products?.length; i++){
       this.nbrArticle = (this.products.length)
     }
+    this.webSocketService.listen('chat').subscribe((data) => this.updateMessage(data));
 
-
+    this.getNotificationsByIdRecu(3); 
   }
 
+
+//-------notification code-----------//
+  notifications: any[] = [];
+  notificationListOpened : boolean = false;
+
+  openNotificationMenu(event: MouseEvent): void {
+    event.stopPropagation(); 
+    this.notificationListOpened = !this.notificationListOpened;
+    this.getNotificationsByIdRecu(3); 
+  }
+
+  getNotificationsByIdRecu(idRecu: any) {
+    this.commandeService.getNotificationsByIdRecu(idRecu).subscribe(
+      (res: any) => {
+        this.notifications = res;
+        console.log(this.notifications);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  showNotifications = false;
+
+
+  toggleNotifications() {
+    this.showNotifications = !this.showNotifications;
+  }
+
+
+  messageTyping(): void {
+    this.webSocketService.emit('typing', "test");    
+  }
+
+  sendMessage(): void {
+    this.webSocketService.emit('chat', {
+      message:"msg",
+      handle: "this.userName"
+    }); 
+  }
+
+  updateMessage(data:any) {
+    this.getNotificationsByIdRecu(3)
+    console.log(this.notifications);
+  }
 
 
 
