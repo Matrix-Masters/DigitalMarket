3d07b492517df733a23a8520d6fce7d388593182
TalelMejri <talelmejri8@gmail.com>
2023-12-29 23:45:47 +0100

    redirect to keycloack test

--- AngularFront/src/app/app.component.ts
+++ AngularFront/src/app/app.component.ts
@@ -1,18 +1,42 @@
-import { Component ,OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import Aos from 'aos';
+import { KeycloakService } from 'keycloak-angular';
+import { KeycloakProfile } from 'keycloak-js';
+import { SecurityServiceService } from './Service/security-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
-export class AppComponent implements  OnInit {
+export class AppComponent implements OnInit {
+  
+  public isLoggedin: boolean = false;
+  public userProfile:any;
 
   title = 'AngularFront';
 
-  ngOnInit(): void {
+  constructor(private keycloakService: KeycloakService,private SecurityServiceService:SecurityServiceService) {}
+
+  ngOnInit() {
     Aos.init();
+    this.SecurityServiceService.init();
+    if(this.keycloakService.isLoggedIn()){
+      this.keycloakService.loadUserProfile().then( async profile=>{
+        this.userProfile=profile.username;
+        this.isLoggedin = true;
+      });
+    }  
+  }
+
+  async login() {
+    await this.keycloakService.login({
+      redirectUri: window.location.origin
+    });
+  }
+
+  public logout() {
+    this.keycloakService.logout();
   }
 
 }
