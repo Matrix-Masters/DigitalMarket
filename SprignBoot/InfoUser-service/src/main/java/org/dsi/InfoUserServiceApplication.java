4311c3be20cddd6085b7b52e032f1ae4aa19999c
TalelMejri <talelmejri8@gmail.com>
2023-12-31 00:10:03 +0100

    getRestTemplate

--- SprignBoot/InfoUser-service/src/main/java/org/dsi/InfoUserServiceApplication.java
+++ SprignBoot/InfoUser-service/src/main/java/org/dsi/InfoUserServiceApplication.java
@@ -3,6 +3,8 @@ package org.dsi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableFeignClients
 @SpringBootApplication
@@ -12,4 +14,8 @@ public class InfoUserServiceApplication {
 		SpringApplication.run(InfoUserServiceApplication.class, args);
 	}
 
+	 	@Bean
+	    public RestTemplate getRestTemplate() {
+	    	return new RestTemplate();
+	    }
 }
