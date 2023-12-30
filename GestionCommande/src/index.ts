1f9cfd53492a682b5eed44061caeb0d90ab89d51
TalelMejri <talelmejri8@gmail.com>
2023-12-31 00:07:55 +0100

    init config keycloack in index

--- GestionCommande/src/index.ts
+++ GestionCommande/src/index.ts
@@ -1,15 +1,21 @@
 import app from "./config/app";
-import ConnectMongo from "./config/DbConnect"
+import ConnectMongo from "./config/DbConnect";
 import router from "./Routes";
 const eurekaHelper = require('./Setup_Euruka');
 const PORT = process.env.PORT || 3000;
-const uri:string='mongodb://127.0.0.1:27017/GestionCommande'
+const uri:string='mongodb://127.0.0.1:27017/GestionCommande';
 const connect=new ConnectMongo(uri);
+import config from "./config/KeycloackConfig";
 
  connect.ConnectToBd().then(()=>{
+
+   app.use(config.session({ secret: 'DigitalMarket', resave: false, saveUninitialized: true }));
+   app.use(config.keycloak.middleware());
+
    app.use("/Commande",router);
    connect.server(app,PORT);
    eurekaHelper.registerWithEureka('GestionCommande-service', PORT);//enregistrer dans Eureka
+   
  }).catch((error:any)=>{
    console.log(error);
  })
