95548ea2df5a89a60cb03080d9892f321efe164c
Youssef Souissi <souissiyoussef9@gmail.com>
2024-01-05 19:31:36 +0100

    update changes

---
+++ mobile/app/src/main/java/com/example/digitalmarket/Client/CheckoutActivity.kt
@@ -0,0 +1,14 @@
+package com.example.digitalmarket.Client
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.digitalmarket.R
+
+class CheckoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.checkout_client)
+
+
+    }
+}
---
+++ mobile/app/src/main/java/com/example/digitalmarket/FournisseurIntoScreen2.kt
@@ -0,0 +1,27 @@
+package com.example.digitalmarket
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityOptionsCompat
+
+class FournisseurIntoScreen2 : AppCompatActivity() {
+    lateinit var navBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fournisseurintroscreen2)
+        navBtn = findViewById(R.id.introButton)
+
+        navBtn.setOnClickListener() {
+            val intent = Intent(this, FournisseurIntroScreen3::class.java)
+            val slideLeftAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_left)
+            navBtn.startAnimation(slideLeftAnimation)
+
+
+            startActivity(intent, ActivityOptionsCompat.makeCustomAnimation(this, R.anim.slide_in_right, R.anim.slide_out_left).toBundle())
+        }
+    }
+}
