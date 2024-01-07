0065b1e0f854617dca4d442c8b79fa54fd24ff46
ghassen <ghassenahmed74@gmail.com>
2024-01-08 00:43:58 +0100

    add verify mail

---
+++ mobile/app/src/main/java/com/example/digitalmarket/signup/VerifyMail.kt
@@ -0,0 +1,12 @@
+package com.example.digitalmarket.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.digitalmarket.R
+
+class VerifyMail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verify_mail)
+    }
+}
