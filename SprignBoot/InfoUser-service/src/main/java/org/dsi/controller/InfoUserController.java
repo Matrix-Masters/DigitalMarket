cbcd18fc1fc5880f32eee16b3f783070647175fe
ghassen <ghassenahmed74@gmail.com>
2024-01-07 16:56:53 +0100

    add column code

--- SprignBoot/InfoUser-service/src/main/java/org/dsi/controller/InfoUserController.java
+++ SprignBoot/InfoUser-service/src/main/java/org/dsi/controller/InfoUserController.java
@@ -156,6 +156,7 @@ public class InfoUserController {
 	 			}
 		    }
 	 	   
+	 	  
 	
 	
 
--- SprignBoot/InfoUser-service/src/main/java/org/dsi/entity/InfoUser.java
+++ SprignBoot/InfoUser-service/src/main/java/org/dsi/entity/InfoUser.java
@@ -80,6 +80,8 @@ public class InfoUser {
 	private Date email_verified_at;
 	
 	private String keycloak_id; 
+	
+	private String code;
 
 	
 }
