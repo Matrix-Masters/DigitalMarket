a4cf1e66909560f7fa62261b6f038d137897aa3b
hkimi amin <hkimiamin02@gmail.com>
2024-01-07 01:46:51 +0100

    fix bugs

--- SprignBoot/product-service/src/main/java/org/dsi/entity/Product.java
+++ SprignBoot/product-service/src/main/java/org/dsi/entity/Product.java
@@ -1,72 +1,72 @@
-package org.dsi.entity;
-
-import java.sql.Timestamp;
-import java.util.List;
-import java.util.Set;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import com.sun.istack.Nullable;
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import net.minidev.json.annotate.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Builder.Default;
-
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
-
-public class Product {
+	package org.dsi.entity;
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
- 	private Long id;
-    
-    private String Name;
-    
-    private String description;
-    
-    private String ImageProduct;
-    
-    private int status=0;
-   
-    private int Quantite;
-    
-    private double prix;
-    
-    private long IdUser=0;
-    
-    private long nbSales=0;
-    
-    @CreationTimestamp
-	private Timestamp created_at;
+	import java.sql.Timestamp;
+	import java.util.List;
+	import java.util.Set;
 	
-	@UpdateTimestamp
-	private Timestamp updated_at;
-
-    /*@OneToMany(fetch = FetchType.EAGER, mappedBy = "product")
-    private List<ProductImages> productImages;*/
+	import javax.persistence.CascadeType;
+	import javax.persistence.Entity;
+	import javax.persistence.FetchType;
+	import javax.persistence.GeneratedValue;
+	import javax.persistence.GenerationType;
+	import javax.persistence.Id;
+	import javax.persistence.JoinColumn;
+	import javax.persistence.ManyToOne;
+	import javax.persistence.OneToMany;
+	import javax.persistence.Table;
 	
-    @Nullable
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "category_id")
-    private Category category;
-    
-    
-}
+	import org.hibernate.annotations.CreationTimestamp;
+	import org.hibernate.annotations.UpdateTimestamp;
+	
+	import com.sun.istack.Nullable;
+	
+	import lombok.Data;
+	import lombok.NoArgsConstructor;
+	import net.minidev.json.annotate.JsonIgnore;
+	import lombok.AllArgsConstructor;
+	import lombok.Builder.Default;
+	
+	
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
+	@Entity
+	
+	public class Product {
+		
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+	 	private Long id;
+	    
+	    private String Name;
+	    
+	    private String description;
+	    
+	    private String ImageProduct;
+	    
+	    private int status=0;
+	   
+	    private int Quantite;
+	    
+	    private double prix;
+	    
+	    private long IdUser=0;
+	    
+	    private long nbSales=0;
+	    
+	    @CreationTimestamp
+		private Timestamp created_at;
+		
+		@UpdateTimestamp
+		private Timestamp updated_at;
+	
+	    /*@OneToMany(fetch = FetchType.EAGER, mappedBy = "product")
+	    private List<ProductImages> productImages;*/
+		
+	    @Nullable
+	    @ManyToOne(fetch = FetchType.EAGER)
+	    @JoinColumn(name = "category_id")
+	    private Category category;
+	    
+	    
+	}
