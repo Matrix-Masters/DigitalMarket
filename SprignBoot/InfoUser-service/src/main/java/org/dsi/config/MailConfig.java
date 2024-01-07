7a393eb7b511440bc47fb75a83f352a585d957c6
ghassen <ghassenahmed74@gmail.com>
2024-01-07 16:56:38 +0100

    change config mail

--- SprignBoot/InfoUser-service/src/main/java/org/dsi/config/MailConfig.java
+++ SprignBoot/InfoUser-service/src/main/java/org/dsi/config/MailConfig.java
@@ -14,9 +14,8 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("hkimiamin02@gmail.com");
-        mailSender.setPassword("vxrnpfhjkmtcsups");
-
+        mailSender.setUsername("villageconnectt@gmail.com");
+        mailSender.setPassword("tfkwbkysyeyzmkmx");
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
