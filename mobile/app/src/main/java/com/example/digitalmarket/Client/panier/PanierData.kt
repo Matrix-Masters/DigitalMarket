1de4a56d963dc8b7e1d6b7f4c860fa7c1358c2e8
TalelMejri <talelmejri8@gmail.com>
2024-01-07 09:26:33 +0100

    add data class credentials

--- mobile/app/src/main/java/com/example/digitalmarket/Client/panier/PanierData.kt
+++ mobile/app/src/main/java/com/example/digitalmarket/Client/panier/PanierData.kt
@@ -1,4 +1,9 @@
 package com.example.digitalmarket.Client.panier
 
-data class PanierData(val nameProduct:String, val photo:Int, var qte:Int, val prix:Double ) {
+data class PanierData(
+        val nameProduct:String,
+        val photo:Int,
+        var qte:Int,
+        val prix:Double
+) {
 }
